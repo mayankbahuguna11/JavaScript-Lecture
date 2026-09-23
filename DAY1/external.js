1a4dc77b2c43f4507c93c281c3601c41da69b736
mayankbahuguna11 <bahugunamayank23@gmail.com>
2026-09-23 16:56:50 +0530

    uploading new changes

---
+++ DAY1/external.js
@@ -0,0 +1,5 @@
+console.log("Hello there");
+// JavaScript is a programming language used to make web pages interactive. 
+// It is a versatile, high-level programming language that serves as one of the core technologies of the world wide web, alongside HTML and CSS. 
+// It is case-sensitive and it is dynamically typed (variable types are determined at runtime). 
+// It executes one task at a time (single-threaded).
